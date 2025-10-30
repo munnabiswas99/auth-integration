d9113bc3fd664096b5b4cf95db0a88701c3e9b83
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-10-30 23:58:50 +0600

    Router Setup Done

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello React</h1>
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
