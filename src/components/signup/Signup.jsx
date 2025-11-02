6ec2e9a28b9ef25002d4c3370a1778a8007f760e
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-02 23:51:53 +0600

    Signup and login form created

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Signup = () => {
-    return (
-        <div>
-            <h1>This is signup</h1>
-        </div>
-    );
+  return (
+    <div className="card bg-base-100 w-full mx-auto mt-20 max-w-sm shrink-0 shadow-2xl">
+      <h1 className="text-5xl font-bold ml-5">Signup Here!</h1>
+      <div className="card-body">
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
