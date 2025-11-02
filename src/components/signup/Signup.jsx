9cdf902226c4373418908d05dc8e5f6b3ea23867
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-03 00:22:39 +0600

    Authentication email pass setup

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import { Link } from "react-router";
 
 const Signup = () => {
+
+    const handleSignup = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log(name, email, password);
+    }
+
   return (
     <div className="card bg-base-100 w-full mx-auto mt-20 max-w-sm shrink-0 shadow-2xl">
       <h1 className="text-5xl font-bold ml-5">Signup Here!</h1>
       <div className="card-body">
-        <form className="fieldset">
+        <form onSubmit={handleSignup} className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" name='name' className="input" placeholder="Name" />
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" name='email' className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name='password' className="input" placeholder="Password" />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
