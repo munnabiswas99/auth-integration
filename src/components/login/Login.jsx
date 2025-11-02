7b95684cfc53408c505dbf7bf8be4a49b70499d0
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-02 23:58:35 +0600

    Link created between signup and login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
   return (
     <div className="card bg-base-100 w-full mx-auto mt-20 max-w-sm shrink-0 shadow-2xl">
       <h1 className="text-5xl font-bold ml-5">Login now!</h1>
       <div className="card-body">
-        <fieldset className="fieldset">
+        <form className="fieldset">
           <label className="label">Email</label>
           <input type="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
@@ -14,7 +15,8 @@ const Login = () => {
             <a className="link link-hover">Forgot password?</a>
           </div>
           <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+        </form>
+        <p>Don't have an accoun? <Link to='/signup' className="text-blue-500">Signup</Link></p>
       </div>
     </div>
   );
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Signup = () => {
   return (
     <div className="card bg-base-100 w-full mx-auto mt-20 max-w-sm shrink-0 shadow-2xl">
       <h1 className="text-5xl font-bold ml-5">Signup Here!</h1>
       <div className="card-body">
-        <fieldset className="fieldset">
+        <form className="fieldset">
           <label className="label">Email</label>
           <input type="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
@@ -13,8 +14,9 @@ const Signup = () => {
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
-          <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+          <button className="btn btn-neutral mt-4">SignUp</button>
+        </form>
+        <p>Already have an accoun? <Link to='/login' className="text-blue-500">Login</Link></p>
       </div>
     </div>
   );
