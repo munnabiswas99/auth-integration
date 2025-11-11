685c01c6e6d9d1d896103253429ef3c7579a904b
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-11 23:26:56 +0600

    Use context API

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createContext, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
-import Home from './components/home/Home.jsx';
-import Signup from './components/signup/Signup.jsx';
-import Login from './components/login/Login.jsx';
-import Root from './components/root/Root.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./components/home/Home.jsx";
+import Signup from "./components/signup/Signup.jsx";
+import Login from "./components/login/Login.jsx";
+import Root from "./components/root/Root.jsx";
+
+export const AuthContext = createContext(null);
+const userInfo = {
+  email: "munna@gmail.com",
+};
 
 let router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
-    children:[
+    children: [
       {
         index: true,
-        Component: Home
+        Component: Home,
       },
 
       {
-        path: '/signup',
-        Component: Signup
+        path: "/signup",
+        Component: Signup,
       },
 
       {
-        path: '/login',
-        Component: Login
-      }
-
-    ]
+        path: "/login",
+        Component: Login,
+      },
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <AuthContext value={userInfo}>
+      <RouterProvider router={router} />
+    </AuthContext>
+  </StrictMode>
+);
